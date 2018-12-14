504a2a8e9c8000d3b19e40fe1884b5e3844f20b6
sharadbhat <sharad.mbhat@gmail.com>
2018-12-14 15:08:06 +0530

    Create Credit.js

---
+++ src/Credit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Credit = (props) => {
+  return (
+    <div className='credit'>
+      Icon made by Smashicons from Flaticon is licensed by CC 3.0 BY
+    </div>
+  )
+}
+
+export default Credit;
